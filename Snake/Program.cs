0fa02396dbf31abcb773d44392deb1a6c26b993f
Makasinych <makasinka@tut.by>
2016-04-27 14:05:28 +0300

    vLine_hLine

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -17,31 +17,12 @@ namespace Snake
             Point p2 = new Point( 4, 5, '#' );
            
             p2.Draw();
+                       
+            HorizontalLine Line = new HorizontalLine(5, 10, 8, '+');
+            Line.Drow();
 
-            Point p3 = new Point(3, 1, '&');
-
-            p3.Draw();
-
-            List<char> numList = new List<char>();
-            numList.Add('^');
-            numList.Add('$');
-            numList.Add('@');
-
-            foreach(char i in numList)
-            {          
-                Console.WriteLine(i);               
-            }
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            numList.Remove('0');
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
+            VerticalLine vLine = new VerticalLine(6, 2, 10, '@');
+            vLine.Drow();
 
             Console.ReadLine();
 
